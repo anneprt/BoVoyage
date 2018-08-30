41e4470a6917d3c8c896d18eb08a92fad398c43a
anneprt <anne.parrot82@gmail.com>
2018-08-30 09:55:10 +0200

    Découpage en couches Application et Core, création des fichiers Data, Entites et Services. Création de la classe Contexte et installation du package NuGet.

---
+++ BoVoyage.Core/Data/Contexte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.Core.Data
+{
+	public class Contexte :DbContext
+	{
+		public 
+	}
+}
