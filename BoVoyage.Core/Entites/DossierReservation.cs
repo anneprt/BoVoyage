1bf6a70ad0ed87335cf527342c06f0aff0974982
ashwingeur <lauravivien@hotmail.fr>
2018-08-30 10:39:47 +0200

    Creation et implementation des classes metier

---
+++ BoVoyage.Core/Entites/DossierReservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.Core.Entites
+{
+    public class DossierReservation
+    {
+        public int Id { get; set; }
+        public int NumeroUnique { get; set; }
+        public string NumeroCarteBancaire { get; set; }
+        public double PrixParPersonne { get; set; }
+        public double PrixTotal { get; set; }
+
+        public static void Annuler() { }
+        public static void ValiderSolvabilite() { }
+        public static void Accepter() { }
+
+
+    }
+}
