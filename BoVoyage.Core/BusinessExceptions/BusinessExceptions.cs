8b1b6c635a534fd0545b94178ada99b605b2277f
anneprt <anne.parrot82@gmail.com>
2018-08-30 11:09:25 +0200

    Creation et implementation dossier et classe BusinessExceptions

---
+++ BoVoyage.Core/BusinessExceptions/BusinessExceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage.Core.BusinessExceptions
+{
+	public class BusinessExceptions :Exception
+	{
+		public BusinessException(string message)
+		   : base(message)
+		{ }
+	}
+}
